4f89c5aeeeeefe54b869a901a3f16c43973d569b
mungai-joel <mungaijoel123@gmail.com>
2021-01-31 22:42:28 +0300

    added all classes and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuotesComponent } from './quotes/quotes.component';
+import { QuotesDetailsComponent } from './quotes-details/quotes-details.component';
+import { QuotesFormComponent } from './quotes-form/quotes-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuotesComponent,
+    QuotesDetailsComponent,
+    QuotesFormComponent
   ],
   imports: [
     BrowserModule,
