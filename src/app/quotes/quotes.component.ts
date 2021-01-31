7b0bfc6015e9cf00d47f365c4e8e2d56dd8d6d8b
mungai-joel <mungaijoel123@gmail.com>
2021-01-31 23:49:03 +0300

    imported necessary components

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,3 +1,4 @@
+import { Quote } from './../quotes';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
+  quotes: Quote[] = [
+    new Quote(
+      'Coders are fueled by Morning coffee',
+      'David Baldaci',
+      'Moonguy',
+      new Date(2020, 3, 31)
+    ),
+    new Quote(
+      'Lms is not a Bible',
+      'David goliath',
+      'man moon',
+      new Date(2020, 3, 31)
+    ),
+    new Quote(
+      'Prepare to master the internet',
+      'Gynn Glen',
+      'King tran',
+      new Date(2020, 3, 31)
+    )
+  ];
 
-  constructor() { }
+  toggleDetails(index) {
+    this.quotes[index].showDescription = !this.quotes[index].showDescription;
+  }
+  deleteQuote(isComplete, index) {
+    if (isComplete) {
+      let toDelete = confirm(
+        `Are you sure you want to delete ${this.quotes[index].name}?`
+      );
 
-  ngOnInit(): void {
+      if (toDelete) {
+        this.quotes.splice(index, 1);
+      }
+    }
+  }
+  addNewQuote(quote) {
+    let quoteLength = this.quotes.length;
+    quote.id = quoteLength + 1;
+    quote.completeDate = new Date(quote.completeDate);
+    this.quotes.push(quote);
   }
 
+  constructor() {}
+
+  ngOnInit() {}
 }
